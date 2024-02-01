8d97e44cc2fbd8d0bd48a75e4f2ad05f4611327a
devstream <devstream@merico.dev>
2024-02-01 17:39:56 +0000

    init by devstream

---
+++ service/src/main/java/io/nuvalence/user/management/api/service/events/PublisherTopic.java
@@ -0,0 +1,11 @@
+package io.nuvalence.user.management.api.service.events;
+
+import lombok.Getter;
+
+/**
+ * Enumerates the topics that can be published to.
+ */
+@Getter
+public enum PublisherTopic {
+    APPLICATION_ROLE_REPORTING
+}
